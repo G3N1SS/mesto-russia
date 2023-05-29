59376a3a630d4cb9b25a9eeafac53653fa1c7de3
Areg Sarkisyan <areg.sarkisyan.99@mail.ru>
2023-05-29 08:45:58 +0300

    first commit

---
+++ blocks/popup/popup.js
@@ -0,0 +1,43 @@
+document.querySelector(".profile__info_edit-button").addEventListener("click",function(){
+    document.getElementById("popup-page").classList.add("popup_opened")
+})
+
+document.querySelector(".popup__close").addEventListener("click",function(){
+    document.getElementById("popup-page").classList.remove("popup_opened")
+})
+
+document.querySelector(".popup__save").addEventListener("click",function(){
+    document.getElementById("popup-page").classList.remove("popup_opened")
+})
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") {
+        document.getElementById("popup-page").classList.remove("popup_opened")
+    }
+});
+
+document.querySelector(".popup .popup__box").addEventListener("click", event => {
+    event._isClickWithInPopup = true;
+});
+document.getElementById("popup-page").addEventListener("click", event => {
+    if (event._isClickWithInPopup) return;
+    event.currentTarget.classList.remove("popup_opened");
+})
+
+let firstName = document.querySelector(".profile__info_name");
+let job = document.querySelector(".profile__info_rank");
+
+document.querySelector('.popup__name').defaultValue = firstName.textContent;
+document.querySelector('.popup__job').defaultValue = job.textContent;
+
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__name');
+let jobInput = document.querySelector('.popup__job');
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+    firstName.textContent = nameInput.value;
+    job.textContent = jobInput.value;
+}
+formElement.addEventListener('submit', handleFormSubmit);
+

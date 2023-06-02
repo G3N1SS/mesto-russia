c2025e4855ca37b7b2796183dc02b57260b7c301
Areg Sarkisyan <areg.sarkisyan.99@mail.ru>
2023-06-02 21:08:45 +0300

    last commit

--- script/index.js
+++ script/index.js
@@ -5,26 +5,28 @@ const popupSave = popup.querySelector(".popup__save")
 const firstName = document.querySelector(".profile__name");
 const job = document.querySelector(".profile__job");
 const formElement = popup.querySelector('.popup__form');
-const nameInput = popup.querySelector('.popup__input_name');
-const jobInput = popup.querySelector('.popup__input_job');
-
-profileEditButton.addEventListener("click",function(){
-    popup.classList.add("popup_opened")
-});
-
-popupClose.addEventListener("click",function(){
-    popup.classList.remove("popup_opened")
-});
+const nameInput = popup.querySelector('.popup__input_type_name');
+const jobInput = popup.querySelector('.popup__input_type_job');
 
 nameInput.value = firstName.textContent;
 jobInput.value = job.textContent;
 
+function popupOpen(){
+    popup.classList.add("popup_opened")
+};
+
+profileEditButton.addEventListener ("click", popupOpen);
+
+function popupClosed(){
+    popup.classList.remove("popup_opened")
+};
+
+popupClose.addEventListener("click", popupClosed);
+
 function handleFormSubmit(evt) {
     evt.preventDefault();
     firstName.textContent = nameInput.value;
-    job.textContent = jobInput.value;   
-}
+    job.textContent = jobInput.value;
+    popupClosed();
+};
 formElement.addEventListener('submit', handleFormSubmit);
-popupSave.addEventListener("click",function(){
-    popup.classList.remove("popup_opened")
-}); 
